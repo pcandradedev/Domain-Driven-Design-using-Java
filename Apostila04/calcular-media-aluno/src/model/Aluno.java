523c0e1695d899f2f511e0e790d444901b3d0218
pcandradedev <pcandradedev@gmail.com>
2026-03-25 18:57:52 -0300

    Pratica 1 - Projeto sugerido para treino sobre (métodos)

--- Apostila04/calcular-media-aluno/src/model/Aluno.java
+++ Apostila04/calcular-media-aluno/src/model/Aluno.java
@@ -1,4 +1,12 @@
 package model;
 
 public class Aluno {
+
+    public String nomeAluno;
+    public String matricula;
+
+
+    public void calcularMedia(double nota1, double nota2, double nota3) {
+        double media = (nota1 + nota2 + nota3) / 3;
+    }
 }
--- Apostila04/calcular-media-aluno/src/view/Terminal.java
+++ Apostila04/calcular-media-aluno/src/view/Terminal.java
@@ -1,4 +1,19 @@
 package view;
 
+import model.Aluno;
+
+import java.util.Scanner;
+
 public class Terminal {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        Aluno aluno = new Aluno();
+
+        System.out.println("Digite o nome do aluno: ");
+        aluno.nomeAluno = entrada.next() + entrada.nextLine();
+
+        System.out.println();
+
+    }
 }
