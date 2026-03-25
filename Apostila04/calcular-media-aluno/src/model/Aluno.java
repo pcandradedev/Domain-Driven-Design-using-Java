ddf4fe4de230efb01e956c3425fd28fc4d141742
pcandradedev <pcandradedev@gmail.com>
2026-03-25 19:08:54 -0300

    Prática 1 - Calcular média

--- Apostila04/calcular-media-aluno/src/model/Aluno.java
+++ Apostila04/calcular-media-aluno/src/model/Aluno.java
@@ -6,7 +6,8 @@ public class Aluno {
     public String matricula;
 
 
-    public void calcularMedia(double nota1, double nota2, double nota3) {
+    public double calcularMedia(double nota1, double nota2, double nota3) {
         double media = (nota1 + nota2 + nota3) / 3;
+        return media;
     }
 }
--- Apostila04/calcular-media-aluno/src/view/Terminal.java
+++ Apostila04/calcular-media-aluno/src/view/Terminal.java
@@ -13,7 +13,22 @@ public class Terminal {
         System.out.println("Digite o nome do aluno: ");
         aluno.nomeAluno = entrada.next() + entrada.nextLine();
 
-        System.out.println();
+        System.out.println("Digite a matrícula do aluno: ");
+        aluno.matricula = entrada.next() + entrada.nextLine();
+
+        System.out.println("Digite o nota do aluno: ");
+        double nota1 = entrada.nextDouble();
+        System.out.println("Digite o nota do aluno: ");
+        double nota2 = entrada.nextDouble();
+        System.out.println("Digite o nota do aluno: ");
+        double nota3 = entrada.nextDouble();
+
+        double media = aluno.calcularMedia(nota1, nota2, nota3);
+
+        System.out.println("Mostrando informações : ");
+        System.out.println("Nome: " + aluno.nomeAluno);
+        System.out.println("Matrícula: " + aluno.matricula);
+        System.out.println("Média do aluno: " + aluno.nomeAluno + " é " +media);
 
     }
 }
